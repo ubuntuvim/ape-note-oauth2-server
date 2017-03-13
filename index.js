68693b63ce2b18b4ba0f1bd9090954ece637bedc
ubuntuvim <1527254027@qq.com>
2017-03-13 16:25:29 +0800

    submit to test

--- index.js
+++ index.js
@@ -30,55 +30,47 @@ app.all('*', function(req, res, next) {
 
 
 app.get('/oauth2/oschina', function(req, res) {
-  console.log('========== /oauth/oschina ============');
+  console.log('========== /oauth2/oschina ============');
   var client_id = "vzCBAoqIVBClvNcXnj";
   var client_secret = "lMUKYuuveVEbhIOolQexMnyzJDmfG";
-  var redirect_uri = "http://test.ape-note.com/oauth/oschina";
-  // 首先获取code
-  var getCodeUrl = `https://www.oschina.net/action/oauth2/authorize?response_type=code&client_id=${client_id}&redirect_uri=${redirect_uri}`;
-  requests(getCodeUrl, function(error, response, body) {
-      console.log(error, response, body);
-      if (!error && response.statusCode === 200) {
-          var bodyObj1 = JSON.parse(body);
-          // 得到code后获取access_token，
-          var code = bodyObj1.code;
-          if (code) {  // 获取access_token
-              var url = `https://www.oschina.net/action/openapi/token?redirect_uri=${redirect_uri}&client_id=${client_id}&client_secret=${client_secret}&grant_type=authorization_code&code=${code}&dataType=json`;
-              request(url, function (error, response, body) {
+  var redirect_uri = "http://oauth2.ape-note.com/oauth/oschina";
+  // 得到code后获取access_token，
+  var code = req.query.code;
+  if (code) {  // 获取access_token
+      var url = `https://www.oschina.net/action/openapi/token?redirect_uri=${redirect_uri}&client_id=${client_id}&client_secret=${client_secret}&grant_type=authorization_code&code=${code}&dataType=json`;
+      request(url, function (error, response, body) {
+          if (!error && response.statusCode === 200) {
+              var bodyObj = JSON.parse(body);
+              var access_token = bodyObj.access_token;
+              // 根据得到的access_token获取用户信息
+              request(`https://www.oschina.net/action/openapi/user?access_token=${access_token}&dataType=json`, function(error, response, body) {
                   if (!error && response.statusCode === 200) {
-                      var bodyObj = JSON.parse(body);
-                      var access_token = bodyObj.access_token;
-                      // 根据得到的access_token获取用户信息
-                      request(`https://www.oschina.net/action/openapi/user?access_token=${access_token}&dataType=json`, function(error, response, body) {
-                          if (!error && response.statusCode === 200) {
-                              var userObj = JSON.parse(body);
-                              res.send({
-                                  msg: '登录成功',
-                                  code: 1,
-                                  user: userObj
-                              });
-                          } else {
-                              res.send({
-                                  msg:"获取用户信息失败: " + error,
-                                  code: 0
-                              });
-                          }
+                      var userObj = JSON.parse(body);
+                      res.send({
+                          msg: '登录成功',
+                          code: 1,
+                          user: userObj
                       });
                   } else {
                       res.send({
-                          msg: "获取`access_token`失败: " + error,
+                          msg:"获取用户信息失败: " + error,
                           code: 0
                       });
                   }
               });
+          } else {
+              res.send({
+                  msg: "获取`access_token`失败: " + error,
+                  code: 0
+              });
           }
-      } else {
-          res.send({
-              msg: "获取`code`失败: " + error,
-              code: 0
-          });
-      }
-  });
+      });
+  } else {
+      res.send({
+          msg: "获取`code`失败: " + error,
+          code: 0
+      });
+  }
 });  // get
 
 app.get('/test', function (req, res) {
